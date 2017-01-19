a7824b23c7ca4af22ee44d26a9df5a25d9eba007
dpmango <xs290@me.com>
2017-01-20 01:56:47 +0300

    new markup

--- src/js/main.js
+++ src/js/main.js
@@ -13,4 +13,57 @@ $(document).ready(function(){
         return false;
 	});
 
+  $('.hamburger').on('click', function(){
+    $(this).toggleClass('is-active');
+    $('.header .menu').toggleClass('active');
+    $('.mobile-menu').toggleClass('active');
+  });
+
+  $('.right_menu_title').on('click', function(){
+    $(this).toggleClass('active');
+    $(this).parent().toggleClass('active');
+  });
+
+  $('.right_menu > ul > .for_price > a').on('click', function(e){
+    e.preventDefault();
+    $(this).parent().toggleClass('active');
+    $(this).parent().find('ul').toggleClass('active');
+  });
+
+  $('.zavod_image-mobile').slick();
+
+  $('.good-mobile-slider').slick({
+    arrows: false,
+    dots: false,
+    responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+  });
+  $('.good-mobile-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+    $('#slickCurrent').text(nextSlide + 1);
+  });
+
+  var countSlides = 1;
+  $.each($('.good-mobile-slider .slick-cloned'), function( index, value ) {
+    countSlides++
+  });
+  // countSlides = (countSlides / 2 ) - 1
+
+  $('#slickTotal').text(countSlides);
+
+  $('.steps-slider').slick();
+
 });
